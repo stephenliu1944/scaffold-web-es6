654d51c9a27318127821d8a9cdc00ce9815818f3
liujunjie <liujunjie@bbdservice.com>
2018-12-09 18:00:17 +0800

    quick commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 var autoprefixer = require('autoprefixer');
-var nested = require('postcss-nested');
 
 module.exports = {
     plugins: [
-        autoprefixer,
-        nested()
+        autoprefixer
     ]
 };
