71df421bee18dbc4a33963316c8a9459bd45b702
Stephen <stephenliu1944@outlook.com>
2019-05-14 10:14:02 +0800

    add @babel/plugin-proposal-pipeline-operator @babel/plugin-syntax-dynamic-import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'images/favicon.ico';
 import 'styles/main.scss';
-import { settings, proxyHost } from '@beancommons/http';
+import http, { helpers } from '@beancommons/http';
 import Home from 'containers/home/Home';
 
 // set global http settings.
-settings({
+http.settings({
     baseURL: __DEV__ && __DOMAIN__,
-    proxyPath: __DEV__ && proxyHost,
+    proxyPath: __DEV__ && helpers.proxyHost,
     isDev: __DEV__
 });
 
